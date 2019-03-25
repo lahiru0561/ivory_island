26d9033a5338ea32b17c192a55e4db91dabc3518
Lahiru Rananasinghe <lahiru0561@gmail.com>
2019-03-26 00:07:42 +1100

    Updates

---
+++ precache-manifest.641e6b843e895e48f9aaf9ee62832f09.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "12b25204e4b3f16142ed",
+    "url": "/ivory_island/static/css/main.4e03c312.chunk.css"
+  },
+  {
+    "revision": "12b25204e4b3f16142ed",
+    "url": "/ivory_island/static/js/main.73211068.chunk.js"
+  },
+  {
+    "revision": "7fc47625265a6e1dbf64",
+    "url": "/ivory_island/static/js/runtime~main.0fa1d888.js"
+  },
+  {
+    "revision": "958384dc87f319e0a91b",
+    "url": "/ivory_island/static/js/2.c5b32edc.chunk.js"
+  },
+  {
+    "revision": "09356babf13e65fc60c760325b0dae1b",
+    "url": "/ivory_island/static/media/img5.09356bab.jpg"
+  },
+  {
+    "revision": "88d7d809b003c33cedf944a0b712fe61",
+    "url": "/ivory_island/static/media/img2.88d7d809.jpg"
+  },
+  {
+    "revision": "73dd5f982dd42286ba8b30d3150bb810",
+    "url": "/ivory_island/static/media/img3.73dd5f98.jpg"
+  },
+  {
+    "revision": "fca4dc9a26b75edb555cbe45e8934a7b",
+    "url": "/ivory_island/static/media/img1.fca4dc9a.jpg"
+  },
+  {
+    "revision": "723641e4a1987c0588f9ad1c3b2e27a6",
+    "url": "/ivory_island/static/media/img4.723641e4.jpg"
+  },
+  {
+    "revision": "aa21abfcf9e1f4fe37580987dc65dcc4",
+    "url": "/ivory_island/static/media/ivory_logo.aa21abfc.jpg"
+  },
+  {
+    "revision": "f7662b760717cd9b8bef5d6eb7a66519",
+    "url": "/ivory_island/static/media/service_1.f7662b76.jpg"
+  },
+  {
+    "revision": "e6eb5a464e65496a07c3c771ae3c742d",
+    "url": "/ivory_island/static/media/service_2.e6eb5a46.jpg"
+  },
+  {
+    "revision": "1a953ed49042e73bbee510dbc9c56616",
+    "url": "/ivory_island/static/media/service_3.1a953ed4.jpg"
+  },
+  {
+    "revision": "a2edd96b73d28de3978e4c88aa0df1fb",
+    "url": "/ivory_island/index.html"
+  }
+];
